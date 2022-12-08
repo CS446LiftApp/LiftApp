a0df57b8dbc14eb26ecf5761c26461af23bfd528
CS446LiftApp <117954004+CS446LiftApp@users.noreply.github.com>
2022-12-08 11:46:51 -0800

    overhauled custom workout page

--- app/src/main/java/com/example/liftingapp/Custom.java
+++ app/src/main/java/com/example/liftingapp/Custom.java
@@ -5,24 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.AdapterView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Custom extends AppCompatActivity {
     private Button toHome;
     private Button toPlanner;
     private Button toGlossary;
-    private Button crunches;
+    private Button done;
+    int currentSelection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom);
 
-        // add crunches task
-        crunches = (Button) findViewById(R.id.crunch);
-        crunches.setOnClickListener(new View.OnClickListener() {
+        final List<String> exercises = Arrays.asList("+", "Arnold Press", "Back Extension", "Back Squat", "Bench Press", "Biceps Curl", "Chest Flys", "Crunches", "Deadlift", "Lateral Raise", "Leg Curls", "Leg Press", "Lunge", "Military Press", "Pull-Downs", "Pull-Ups", "Push-Downs", "Push-Ups", "Shoulder Press", "Triceps Dips");
+        Spinner spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.spinner, exercises);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        currentSelection = spinner.getSelectedItemPosition();
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
            @Override
-           public void onClick(View view) { addCrunches(); }
+           public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
+               if (currentSelection != i) { addedItem(); }
+               currentSelection = i;
+           }
+
+           @Override
+           public void onNothingSelected(AdapterView<?> parent) {
+               // do nothing
+           }
+        });
+
+        // done button
+        done = (Button) findViewById(R.id.done_button);
+        done.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) { openHome(); }
         });
 
         // bottom bar navigation buttons
@@ -66,7 +95,7 @@ public class Custom extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void addCrunches() {
+    public void addedItem() {
         Intent intent = new Intent(this, CustomAddFirst.class);
         startActivity(intent);
     }
